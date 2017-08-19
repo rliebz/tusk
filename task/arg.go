a916bf2c1f7824b458896627d9ec7f8b8e739986
rliebz <rliebz@gmail.com>
2017-08-18 21:07:44 -0400

    Reenable running commands to compute variables

--- task/arg.go
+++ task/arg.go
@@ -3,10 +3,12 @@ package task
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
 	"gitlab.com/rliebz/tusk/appyaml"
 
+	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 )
 
@@ -18,17 +20,28 @@ type Arg struct {
 	Private bool
 
 	// Used to determine value, in order of highest priority
+	// `Command` and `Default` are mutually exclusive
 	Passed      string `yaml:"-"`
 	Environment string
-	Computed    []struct {
-		When  appyaml.When
-		Value string
-	}
-	Default string
+	Computed    []computed
+	Command     string
+	Default     string
 
 	// Computed members not specified in yaml file
 	Name string `yaml:"-"`
 }
+
+func (a *Arg) getCommand() string { return a.Command }
+func (a *Arg) getDefault() string { return a.Default }
+
+type computed struct {
+	When    appyaml.When
+	Command string
+	Default string
+}
+
+func (c *computed) getCommand() string { return c.Command }
+func (c *computed) getDefault() string { return c.Default }
 
 // CreateCLIFlag converts an Arg into a cli.Flag.
 func CreateCLIFlag(arg *Arg) (cli.Flag, error) {
@@ -66,25 +79,65 @@ func CreateCLIFlag(arg *Arg) (cli.Flag, error) {
 }
 
 // Value determines the final argument value based on all options.
-func (arg *Arg) Value() (string, error) {
+func (a *Arg) Value() (string, error) {
 
-	if !arg.Private {
-		if arg.Passed != "" {
-			return arg.Passed, nil
+	if !a.Private {
+		if a.Passed != "" {
+			return a.Passed, nil
 		}
 
-		envValue := os.Getenv(arg.Environment)
+		envValue := os.Getenv(a.Environment)
 		if envValue != "" {
 			return envValue, nil
 		}
 	}
 
-	for _, candidate := range arg.Computed {
+	for _, candidate := range a.Computed {
 		if err := candidate.When.Validate(); err != nil {
 			continue
 		}
-		return candidate.Value, nil
+
+		value, err := getCommandOrDefault(&candidate)
+		if err != nil {
+			return "", errors.Wrapf(err, "could not compute value for flag: %s", a.Name)
+		}
+
+		return value, nil
 	}
 
-	return arg.Default, nil
+	value, err := getCommandOrDefault(a)
+	if err != nil {
+		return "", errors.Wrapf(err, "could not compute value for flag: %s", a.Name)
+	}
+
+	return value, nil
+}
+
+// valueGetter determines a value by running a command or using a default.
+// While both options are available, it is required that only one of the two
+// are defined, since neither has an innate priority over the other.
+type valueGetter interface {
+	getCommand() string
+	getDefault() string
+}
+
+func getCommandOrDefault(vg valueGetter) (string, error) {
+
+	if vg.getDefault() != "" && vg.getCommand() != "" {
+		return "", fmt.Errorf(
+			"default (%s) and command (%s) are both defined",
+			vg.getDefault(), vg.getCommand(),
+		)
+	}
+
+	if vg.getCommand() != "" {
+		out, err := exec.Command("sh", "-c", vg.getCommand()).Output() // nolint: gas
+		if err != nil {
+			return "", err
+		}
+
+		return strings.TrimSpace(string(out)), nil
+	}
+
+	return vg.getDefault(), nil
 }
