a44d61df24ccc86abe53087a91ef4fcf51cf6091
Zach Liebowitz <zach@Zliebowitz.com>
2017-10-22 17:39:22 -0400

    Remove read access to ui verbosity variables

--- ui/output.go
+++ ui/output.go
@@ -19,7 +19,7 @@ func Print(a ...interface{}) {
 
 // Debug prints info only in verbose mode.
 func Debug(a ...interface{}) {
-	if Quiet || !Verbose {
+	if !IsVerbose() {
 		return
 	}
 
@@ -28,7 +28,7 @@ func Debug(a ...interface{}) {
 
 // Info prints application info.
 func Info(a ...interface{}) {
-	if Quiet {
+	if IsQuiet() {
 		return
 	}
 
@@ -37,7 +37,7 @@ func Info(a ...interface{}) {
 
 // Warn prints an application warning.
 func Warn(a ...interface{}) {
-	if Quiet {
+	if IsQuiet() {
 		return
 	}
 	logInStyle(warningString, yellow, a...)
