3c1e2e4881ce124e57bfca00a26ee5cf8cf7c6a3
Robert Liebowitz <rliebz@gmail.com>
2018-03-31 12:29:56 -0400

    Add tests for args and options

---
+++ config/option/arg_test.go
@@ -0,0 +1,112 @@
+package option
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/rliebz/tusk/config/marshal"
+	yaml "gopkg.in/yaml.v2"
+)
+
+func TestEvaluate(t *testing.T) {
+	expected := "foo"
+	arg := Arg{Passed: expected}
+
+	actual, err := arg.Evaluate()
+	if err != nil {
+		t.Fatalf("Arg.Evaluate() => unexpected error: %v", err)
+	}
+
+	if expected != actual {
+		t.Errorf("Arg.Evaluate() => want %q, got %q", expected, actual)
+	}
+}
+
+func TestEvaluate_specified(t *testing.T) {
+	expected := "foo"
+	arg := Arg{
+		Passed: expected,
+		valueWithList: valueWithList{
+			ValuesAllowed: marshal.StringList{"wrong", expected, "other"},
+		},
+	}
+
+	actual, err := arg.Evaluate()
+	if err != nil {
+		t.Fatalf("Arg.Evaluate() => unexpected error: %v", err)
+	}
+
+	if expected != actual {
+		t.Errorf("Arg.Evaluate() => want %q, got %q", expected, actual)
+	}
+}
+
+func TestEvaluate_unspecified(t *testing.T) {
+	passed := "foo"
+	arg := Arg{
+		Passed: passed,
+		valueWithList: valueWithList{
+			ValuesAllowed: marshal.StringList{"wrong", "other"},
+		},
+	}
+
+	if _, err := arg.Evaluate(); err == nil {
+		t.Fatal("Arg.Evaluate() => want error for nil argument, got nil")
+	}
+}
+
+func TestEvaluate_nil(t *testing.T) {
+	var arg *Arg
+	if _, err := arg.Evaluate(); err == nil {
+		t.Fatal("Arg.Evaluate() => want error for nil argument, got nil")
+	}
+}
+
+func TestGetArgsWithOrder(t *testing.T) {
+	name := "foo"
+	usage := "foo usage"
+	ms := yaml.MapSlice{
+		{Key: name, Value: &Arg{Usage: usage}},
+		{Key: "bar", Value: &Arg{Usage: "bar usage"}},
+	}
+
+	args, ordered, err := GetArgsWithOrder(ms)
+	if err != nil {
+		t.Fatalf("GetArgsWithOrder(ms) => unexpected error: %v", err)
+	}
+
+	if len(ms) != len(args) || len(ms) != len(ordered) {
+		t.Fatalf(
+			"GetArgsWithOrder(ms) => want %d items, got %d in map and %d in slice",
+			len(ms), len(args), len(ordered),
+		)
+	}
+
+	arg, ok := args[name]
+	if !ok {
+		t.Fatalf("GetArgsWithOrder(ms) => item %q is not in map", name)
+	}
+
+	if name != arg.Name {
+		t.Errorf(
+			"GetArgsWithOrder(ms) => want arg.Name %q, got %q",
+			name, arg.Name,
+		)
+	}
+
+	if usage != arg.Usage {
+		t.Errorf(
+			"GetArgsWithOrder(ms) => want arg.Usage %q, got %q",
+			usage, arg.Usage,
+		)
+	}
+
+	expectedOrder := []string{"foo", "bar"}
+	if !reflect.DeepEqual(expectedOrder, ordered) {
+		t.Errorf(
+			"GetArgsWithOrder(ms) => want ordered %v, got %v",
+			expectedOrder, ordered,
+		)
+	}
+
+}
