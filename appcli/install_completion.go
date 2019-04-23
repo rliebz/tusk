b8d8cf4001147788f0cc7ed7742a3cc9461c5d31
Robert Liebowitz <rliebz@gmail.com>
2019-04-22 23:37:08 -0400

    Install a temporary string to a bash RC file

--- appcli/install_completion.go
+++ appcli/install_completion.go
@@ -1,6 +1,7 @@
 package appcli
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -14,6 +15,8 @@ const zshInstallDir = "/usr/local/share/zsh/site-functions"
 // InstallCompletions installs command line completions for a given shell.
 func InstallCompletions(shell string) error {
 	switch shell {
+	case "bash":
+		return installBashCompletion()
 	case "zsh":
 		return installZshCompletion(zshInstallDir)
 	default:
@@ -29,6 +32,64 @@ func UninstallCompletions(shell string) error {
 	default:
 		return fmt.Errorf("tab completion for %q is not supported", shell)
 	}
+}
+
+// TODO: Write the real command
+const bashCommand = `echo "Hello, world"`
+
+var bashRCFiles = []string{".bashrc", ".bash_profile", ".profile"}
+
+func installBashCompletion() error {
+	rcfile, err := getBashRCFile()
+	if err != nil {
+		return err
+	}
+
+	return appendIfAbsent(rcfile, bashCommand)
+}
+
+func getBashRCFile() (string, error) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	for _, rcfile := range bashRCFiles {
+		path := filepath.Join(homedir, rcfile)
+		if _, err := os.Stat(path); err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+
+			return "", err
+		}
+
+		return path, nil
+	}
+
+	return filepath.Join(homedir, ".bashrc"), nil
+}
+
+func appendIfAbsent(path, text string) error {
+	// nolint: gosec
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close() // nolint: errcheck
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if scanner.Text() == text {
+			return nil
+		}
+	}
+	if serr := scanner.Err(); serr != nil {
+		return serr
+	}
+
+	_, err = fmt.Fprintln(f, text)
+	return err
 }
 
 func installZshCompletion(dir string) error {
--- appcli/install_completion_test.go
+++ appcli/install_completion_test.go
@@ -8,8 +8,110 @@ import (
 
 	"gotest.tools/assert"
 	"gotest.tools/assert/cmp"
+	"gotest.tools/env"
 	"gotest.tools/fs"
 )
+
+func TestGetBashRCFile(t *testing.T) {
+	tests := []struct {
+		name   string
+		ops    []fs.PathOp
+		expect string
+	}{
+		{
+			"no-files",
+			[]fs.PathOp{},
+			".bashrc",
+		},
+		{
+			"all-files",
+			[]fs.PathOp{
+				fs.WithFile(".bashrc", ""),
+				fs.WithFile(".bash_profile", ""),
+				fs.WithFile(".profile", ""),
+			},
+			".bashrc",
+		},
+		{
+			"bash-profile-only",
+			[]fs.PathOp{
+				fs.WithFile(".bash_profile", ""),
+			},
+			".bash_profile",
+		},
+		{
+			"profile-only",
+			[]fs.PathOp{
+				fs.WithFile(".profile", ""),
+			},
+			".profile",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			homedir := fs.NewDir(t, "home", tt.ops...)
+			defer homedir.Remove()
+
+			defer env.PatchAll(t, map[string]string{
+				"HOME":        homedir.Path(),
+				"USERPROFILE": homedir.Path(),
+			})()
+
+			rcfile, err := getBashRCFile()
+			assert.NilError(t, err)
+
+			want := filepath.Join(homedir.Path(), tt.expect)
+			assert.Check(t, cmp.Equal(want, rcfile))
+		})
+	}
+}
+
+func TestAppendIfAbsent(t *testing.T) {
+	existing := "# First Line\n"
+	f := fs.NewFile(t, "bashrc", fs.WithContent(existing))
+	defer f.Remove()
+
+	text := "# Second Line"
+	err := appendIfAbsent(f.Path(), text)
+	assert.NilError(t, err)
+
+	want := existing + text + "\n"
+	got, err := ioutil.ReadFile(f.Path())
+	assert.NilError(t, err)
+
+	assert.Check(t, cmp.Equal(want, string(got)))
+}
+
+func TestAppendIfAbsent_exists(t *testing.T) {
+	text := "# Existing Line"
+	f := fs.NewFile(t, "bashrc", fs.WithContent(text))
+	defer f.Remove()
+
+	err := appendIfAbsent(f.Path(), text)
+	assert.NilError(t, err)
+
+	got, err := ioutil.ReadFile(f.Path())
+	assert.NilError(t, err)
+
+	assert.Check(t, cmp.Equal(text, string(got)))
+}
+
+func TestAppendIfAbsent_no_file(t *testing.T) {
+	f := fs.NewFile(t, "bashrc")
+	defer f.Remove() // Will be recreated
+
+	f.Remove()
+
+	text := "# Target Line"
+	err := appendIfAbsent(f.Path(), text)
+	assert.NilError(t, err)
+
+	got, err := ioutil.ReadFile(f.Path())
+	assert.NilError(t, err)
+
+	assert.Check(t, cmp.Equal(text+"\n", string(got)))
+}
 
 func TestInstallZshCompletion(t *testing.T) {
 	dir := fs.NewDir(t, "project-dir")
