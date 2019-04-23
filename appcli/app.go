1cdb3cf4a368cbf0ac191a1543105a2e7c5ed878
Robert Liebowitz <rliebz@gmail.com>
2019-04-22 22:30:37 -0400

    Hide completion flags from --help menu

--- appcli/app.go
+++ appcli/app.go
@@ -31,12 +31,14 @@ func newBaseApp() *cli.App {
 			Usage: "Set `file` to use as the config file",
 		},
 		cli.StringFlag{
-			Name:  "install-completions",
-			Usage: "Install completions for a `shell`",
+			Name:   "install-completions",
+			Usage:  "Install completions for a `shell`",
+			Hidden: true,
 		},
 		cli.StringFlag{
-			Name:  "uninstall-completions",
-			Usage: "Uninstall completions for a `shell`",
+			Name:   "uninstall-completions",
+			Usage:  "Uninstall completions for a `shell`",
+			Hidden: true,
 		},
 		cli.BoolFlag{
 			Name:  "q, quiet",
